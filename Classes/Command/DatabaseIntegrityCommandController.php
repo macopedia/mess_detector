a996c481dd2108c7e05aeb7c194550c90190de7b
Tymoteusz Motylewski <t.motylewski@gmail.com>
2016-11-21 18:12:20 +0100

    Refactor commands to iterate over all tables defined in TCA

--- Classes/Command/DatabaseIntegrityCommandController.php
+++ Classes/Command/DatabaseIntegrityCommandController.php
@@ -13,74 +13,116 @@ class DatabaseIntegrityCommandController extends CommandController
     /**
      * Finds all records which has wrong t3_origuid value set
      *
+     * @param string $tableName pass table name if you want to run the check on a single table
      * @return void
      * @cli
      */
-    public function checkT3OrigUidCommand()
+    public function checkT3OrigUidEqualsUidCommand($tableName='')
     {
+        foreach ($GLOBALS['TCA'] as $table => $tca) {
+            if (!empty($tableName) && $table != $tableName) {
+                continue;
+            }
+            if (empty($tca['ctrl']['origUid'])) {
+                continue;
+            }
+            $origUidFieldName = $tca['ctrl']['origUid'];
 
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
-        // Remove all default restrictions (delete, hidden, starttime, stoptime), but add DeletedRestriction again
-        $queryBuilder->getRestrictions()
-            ->removeAll();
-        $rows = $queryBuilder
-            ->select('uid', 'pid', 'l18n_parent', 't3_origuid', 'header')
-            ->from('tt_content')
-            ->where(
-                $queryBuilder->expr()->orX(
-                    $queryBuilder->expr()->eq('uid', $queryBuilder->quoteIdentifier('t3_origuid'))
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
+            // Remove all default restrictions (delete, hidden, starttime, stoptime)
+            $queryBuilder->getRestrictions()
+                ->removeAll();
+            $select = ['uid', 'pid', $origUidFieldName];
+            if (!empty($tca['ctrl']['transOrigPointerField'])) {
+                $select[] = $tca['ctrl']['transOrigPointerField'];
+            }
+            if (!empty($tca['ctrl']['delete'])) {
+                $select[] = $tca['ctrl']['delete'];
+            }
+            if (!empty($tca['ctrl']['label'])) {
+                $select[] = $tca['ctrl']['label'];
+            }
+            $rows = $queryBuilder
+                ->select(...$select)
+                ->from($table)
+                ->where(
+                    $queryBuilder->expr()->orX(
+                        $queryBuilder->expr()->eq('uid', $queryBuilder->quoteIdentifier($origUidFieldName))
+                    )
                 )
-            )
-            ->execute()
-            ->fetchAll();
-        if (!empty($rows)) {
-            $this->outputLine('Following records have wrong t3_origuid value. t3_origuid should never be equal to uid');
-            $this->outputLine('Query run:');
-            $this->outputLine($queryBuilder->getSQL());
+                ->execute()
+                ->fetchAll();
 
-            $this->output->outputTable($rows, array_keys($rows[0]));
-        } else {
-            $this->outputLine('All good.');
+            $this->outputLine('Checking table: ' . $table);
+            if (!empty($rows)) {
+                $this->outputLine('Following records have wrong t3_origuid value. t3_origuid should never be equal to uid.');
+                $this->outputLine('It should be safe to set t3_origuid for these records to 0.');
+                $this->outputLine('This issue might be caused by the workspaces bug, see https://forge.typo3.org/issues/78643');
+                $this->outputLine('Query run:');
+                $this->outputLine($queryBuilder->getSQL());
+
+                $this->output->outputTable($rows, array_keys($rows[0]));
+            } else {
+                $this->outputLine('All good.');
+            }
         }
     }
 
     /**
-     * Finds all records which has wrong t3_origuid value set
+     * Finds all records which has l10n_parent value set to uid of the record in non default language
      *
+     * @param string $tableName pass table name if you want to run the check on a single table
      * @return void
      * @cli
      */
-    public function checkL10nParentCommand()
+    public function checkL10nParentCommand($tableName='')
     {
-
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
-        // Remove all default restrictions (delete, hidden, starttime, stoptime), but add DeletedRestriction again
-        $queryBuilder->getRestrictions()
-            ->removeAll();
-        $rows = $queryBuilder
-            ->select('tt_content.uid', 'tt_content.pid', 'tt_content.l18n_parent', 'tt_content.t3_origuid', 'tt_content.header')
-            ->from('tt_content')
-            ->leftJoin(
-                'tt_content',
-                'tt_content',
-                'tt_content_parent',
-                $queryBuilder->expr()->eq(
-                    'tt_content.l18n_parent',
-                     $queryBuilder->quoteIdentifier('tt_content_parent.uid')
+        foreach ($GLOBALS['TCA'] as $table => $tca) {
+            if (!empty($tableName) && $table != $tableName) {
+                continue;
+            }
+            if (empty($tca['ctrl']['transOrigPointerField'])
+                || !empty($tca['ctrl']['transOrigPointerTable'])) {
+                continue;
+            }
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
+            // Remove all default restrictions (delete, hidden, starttime, stoptime)
+            $queryBuilder->getRestrictions()
+                ->removeAll();
+            $select = [$table . '.uid', $table . '.pid', $table . '.' . $tca['ctrl']['transOrigPointerField']];
+            if (!empty($tca['ctrl']['delete'])) {
+                $select[] = $table . '.' . $tca['ctrl']['delete'];
+            }
+            if (!empty($tca['ctrl']['label'])) {
+                $select[] = $table . '.' . $tca['ctrl']['label'];
+            }
+            $rows = $queryBuilder
+                ->select(...$select)
+                ->from($table)
+                ->leftJoin(
+                    $table,
+                    $table,
+                    $table . '_parent',
+                    $queryBuilder->expr()->eq(
+                        $table . '.' . $tca['ctrl']['transOrigPointerField'],
+                        $queryBuilder->quoteIdentifier($table . '_parent.uid')
+                    )
                 )
-            )
-            ->where(
-                $queryBuilder->expr()->neq('tt_content_parent.sys_language_uid', 0)
-            )
-            ->execute()
-            ->fetchAll();
-        if (!empty($rows)) {
-            $this->outputLine('Following records have wrong l18n_parent value.');
-            $this->outputLine('Query run:');
-            $this->outputLine($queryBuilder->getSQL());
-            $this->output->outputTable($rows, array_keys($rows[0]));
-        } else {
-            $this->outputLine('All good.');
+                ->where(
+                    $queryBuilder->expr()->neq($table . '_parent.' . $tca['ctrl']['transOrigPointerField'], 0)
+                )
+                ->execute()
+                ->fetchAll();
+            $this->outputLine('Checking table: ' . $table);
+            if (!empty($rows)) {
+                $this->outputLine('Following records have wrong transOrigPointerField (l10n_parent) value.');
+                $this->outputLine('transOrigPointerField field should always point to a record in the default language (0)');
+                $this->outputLine('Query run:');
+                $this->outputLine($queryBuilder->getSQL());
+                $this->output->outputTable($rows, array_keys($rows[0]));
+            } else {
+                $this->outputLine('All good.');
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,7 @@ TYPO3 Mess Detector
 ===================
 
 TYPO3 tools that will help you find out how messed up your TYPO3 instance is.
+It checks database integrity.
 
 
 
