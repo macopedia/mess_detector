b1a169d06904b4b5b878c86154703f995a48f74a
Tymoteusz Motylewski <t.motylewski@gmail.com>
2016-11-22 15:03:47 +0100

    Add command to check pid of translated records

--- Classes/Command/DatabaseIntegrityCommandController.php
+++ Classes/Command/DatabaseIntegrityCommandController.php
@@ -78,7 +78,7 @@ class DatabaseIntegrityCommandController extends CommandController
      * @return void
      * @cli
      */
-    public function checkL10nParentCommand($tableName='')
+    public function checkL10nParentDefaultLanguageCommand($tableName='')
     {
         foreach ($GLOBALS['TCA'] as $table => $tca) {
             if (!empty($tableName) && $table != $tableName) {
@@ -120,6 +120,64 @@ class DatabaseIntegrityCommandController extends CommandController
             if (!empty($rows)) {
                 $this->outputLine('Following records have wrong transOrigPointerField (l10n_parent) value.');
                 $this->outputLine('transOrigPointerField field should always point to a record in the default language (0)');
+                $this->outputLine('Query run:');
+                $this->outputLine($queryBuilder->getSQL());
+                $this->output->outputTable($rows, array_keys($rows[0]));
+            } else {
+                $this->outputLine('All good.');
+            }
+        }
+    }
+
+    /**
+     * Finds all records which has l10n_parent value set to uid of the record on a different page
+     *
+     * @param string $tableName pass table name if you want to run the check on a single table
+     * @return void
+     * @cli
+     */
+    public function checkL10nParentPidCommand($tableName='')
+    {
+        foreach ($GLOBALS['TCA'] as $table => $tca) {
+            if (!empty($tableName) && $table != $tableName) {
+                continue;
+            }
+            if (empty($tca['ctrl']['transOrigPointerField'])
+                || !empty($tca['ctrl']['transOrigPointerTable'])) {
+                continue;
+            }
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
+            // Remove all default restrictions (delete, hidden, starttime, stoptime)
+            $queryBuilder->getRestrictions()
+                ->removeAll();
+            $select = [$table . '.uid', $table . '.pid', $table . '.' . $tca['ctrl']['languageField'], $table . '.' . $tca['ctrl']['transOrigPointerField']];
+            if (!empty($tca['ctrl']['delete'])) {
+                $select[] = $table . '.' . $tca['ctrl']['delete'];
+            }
+            if (!empty($tca['ctrl']['label'])) {
+                $select[] = $table . '.' . $tca['ctrl']['label'];
+            }
+            $rows = $queryBuilder
+                ->select(...$select)
+                ->from($table)
+                ->leftJoin(
+                    $table,
+                    $table,
+                    $table . '_parent',
+                    $queryBuilder->expr()->eq(
+                        $table . '.' . $tca['ctrl']['transOrigPointerField'],
+                        $queryBuilder->quoteIdentifier($table . '_parent.uid')
+                    )
+                )
+                ->where(
+                    $queryBuilder->expr()->neq($table . '_parent.pid', $table . '.pid')
+                )
+                ->execute()
+                ->fetchAll();
+            $this->outputLine('Checking table: ' . $table);
+            if (!empty($rows)) {
+                $this->outputLine('Following records live on a different page then their translation source transOrigPointerField (l10n_parent)');
+                $this->outputLine('Original record and its translation should live on the same page.');
                 $this->outputLine('Query run:');
                 $this->outputLine($queryBuilder->getSQL());
                 $this->output->outputTable($rows, array_keys($rows[0]));
